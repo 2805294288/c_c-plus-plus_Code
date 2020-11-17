64465d3170a650258fb4b67a461765119058331c
2805294288 <2805294288@qq.com>
2020-11-17 12:54:10 +0800

    字符串按照单个字符逆序输出

---
+++ 字符串按照单个字符逆序输出/源.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+void reverse(char* begin, char* end)
+{
+	while (begin < end)
+	{
+		char temp=*begin
+	}
+}
+int main()
+{
+	char str[100] = { 0 };
+	fgets(str, sizeof(str) - 1,stdin);
+	str[strlen(str) - 1] = '\0';
+	char* begin = str;
+	char* end = str;
+	while (*end != '\0') {
+		while (*end != ' ' && *end != '\0') {
+
+		}
+	}
+
+
+
+}
