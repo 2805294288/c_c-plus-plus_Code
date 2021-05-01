2fc947ef5d05b3ff75938062a893ff7784a7fa87
2805294288 <2805294288@qq.com>
2021-05-01 17:01:02 +0800

    二叉树的层序遍历

---
+++ L-102-Binary Tree Level Order Traversal/code.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ret;
+        queue<TreeNode*> que;
+        if(root)
+        {
+            que.push(root);
+        }
+        
+        while(!que.empty())
+        {
+            vector<int> tmp;
+            int size=que.size();
+            for(int i=0; i<size; i++)
+            {
+                TreeNode* Node =  que.front();
+                que.pop();
+                tmp.push_back(Node->val);
+                if(Node->left)
+                {
+                    que.push(Node->left);
+                }
+                if(Node->right)
+                {
+                    que.push(Node->right);
+                }
+            }
+            ret.push_back(tmp);  
+        }
+        return ret;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
